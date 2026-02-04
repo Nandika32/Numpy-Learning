228e8e028a7c3d9610fe48e59e7e80007bbce4a1
Nandika32 <ptnandika21@gmail.com>
2026-02-05 00:20:57 +0530

    Added examples

---
+++ arrays.py
@@ -0,0 +1,10 @@
+import numpy as np
+a=np.array([10,20,30,40,50])
+print(a)
+b=np.array([[1,2,3],[4,5,6]])
+c=np.array([[7,8,9],[10,11,12]])
+print(b*c)
+print(b+c)
+print(b-c)
+d=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+e=d.reshape(3,4)
